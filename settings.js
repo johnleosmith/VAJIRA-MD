9e24990263109e3c95932b1f909db646b6ba5523
Johnleo😈 <155096892+johnleosmith@users.noreply.github.com>
2024-07-19 00:09:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,10 +8,10 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'JOHNLEO-MD|-WA-BOT™';
 const FOOTERNAME = '> JOHNLEO -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '2349039923259' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '2349165237782' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=7MsWgAiI#K7nC83-kHV2ZA4aISUBY3Uy90M9QdfFFa2eDG9kAX6w' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=qM8FwaqZ#FSXCeyq9N5cHc3Nt-oY7fe9t5-kqQGyqSN2UjPkDNms' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "2349039923259" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
@@ -24,7 +24,7 @@ PREFIX: process.env.PREFIX || '.' ,
 LANG:  process.env.LANG || 'SI' ,
 ANTI_BAD: process.env.ANTI_BAD || false  ,
 AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
-AUTO_TYPING:  process.env.AUTO_TYPING  || true  ,
+AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
 AUTO_RECORDING:  process.env.AUTO_RECORDING  || true  ,
 AUTO_READ:  process.env.AUTO_READ  || false  ,
 AUTO_BIO:  process.env.AUTO_BIO  || true  ,       
