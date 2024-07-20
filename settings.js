71f252ae43ee415b04e172574ca8741ea397c4ec
Johnleo😈 <155096892+johnleosmith@users.noreply.github.com>
2024-07-20 10:16:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,12 +8,12 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'JOHNLEO-MD|-WA-BOT™';
 const FOOTERNAME = '> JOHNLEO -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '2349165237782' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '2349020507509' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bBs3xKxZ#KiCtaC7vxjpuBrUoHiT5VckCzdjF_cV9O8tpGb7dkUg' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=PJ0iQBoZ#-mVYNE-FZXOfxn5_nOwZh9drUOMiDLxUh9DVnB-4BLI' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
-MODERATORS : process.env.MODERATORS === undefined ? "27692333626" : process.env.MODERATORS ,    
+MODERATORS : process.env.MODERATORS === undefined ? "2349020507509" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'JOHNLEO' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
